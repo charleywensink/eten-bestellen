1748f0988b36fc2ccfdce9074b097138f810c020
Jonah88877 <88877@glr.nl>
2023-03-20 13:56:01 +0100

    test

--- bestel/script.js
+++ bestel/script.js
@@ -20,13 +20,25 @@ function displayCart() {
 
     // Clear items
     document.getElementById("items").innerHTML = "";
+    document.getElementById("totalPrice").innerHTML = "";
+    document.getElementById("korting").innerHTML = "";
+
+    let totalPrice = 0
+    let priceAbove50 = 0;
 
     for (let i = 0; i < cart.length; i++) {
         let item = cart[i];
         let li = document.createElement("li");
-        li.textContent = `${item.name} - €${item.price} x ${item.quantity}`;
+        li.innerHTML = `${item.name} - €${item.price} x ${item.quantity}`;
         document.getElementById("items").appendChild(li);
+
+        totalPrice += item.price * item.quantity;
     }
+    // Checkt of de totalprice over 50 is, zo ja dan een 15% korting
+
+    // Laat de prijs en prijs met korting zien
+    document.getElementById("totalPrice").innerHTML = `Totaal: €${totalPrice.toFixed(2)}`;
+    document.getElementById("korting").innerHTML = `Prijs met korting: €${priceAbove50.toFixed(2)}`;
 }
 
 function clearCart() {
@@ -36,6 +48,5 @@ function clearCart() {
     displayCart();
 }
 
-displayCart();
 
 // Code met JSON heb ik van youtube
