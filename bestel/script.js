ebe1715b2160ee3cd7b7cea5bb78a2da4e02b24e
Jonah88877 <88877@glr.nl>
2023-03-20 11:51:06 +0100

    Werkende functie dat item toevoegt aan winkelwagen

--- bestel/script.js
+++ bestel/script.js
@@ -1,5 +1,41 @@
-function bestel() {
+function addItem(name, price) {
+    let cart = JSON.parse(localStorage.getItem("shoppingCart")) || [];
 
+    let existingItemIndex = cart.findIndex(cartItem => cartItem.name === name);
+    if (existingItemIndex !== -1) {
+        cart[existingItemIndex].quantity += 1;
+    } else {
+        cart.push({ name: name, price: price, quantity: 1 });
+    }
 
+    //Save items
+    localStorage.setItem("shoppingCart", JSON.stringify(cart));
 
+    displayCart();
 }
+
+function displayCart() {
+    // Load items
+    let cart = JSON.parse(localStorage.getItem("shoppingCart")) || [];
+
+    // Clear items
+    document.getElementById("items").innerHTML = "";
+
+    for (let i = 0; i < cart.length; i++) {
+        let item = cart[i];
+        let li = document.createElement("li");
+        li.textContent = `${item.name} - €${item.price} x ${item.quantity}`;
+        document.getElementById("items").appendChild(li);
+    }
+}
+
+function clearCart() {
+    // Clear items in storage
+    localStorage.removeItem("shoppingCart");
+
+    displayCart();
+}
+
+displayCart();
+
+// Code met JSON heb ik van youtube
